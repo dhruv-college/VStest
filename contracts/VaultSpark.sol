7e4035e94395f65f3583f77c2dafc5ac3619edd2
dhruv-college <hi2dhruv@gmail.com>
2025-07-25 10:43:15 +0530

    Update VaultSpark.sol

--- contracts/VaultSpark.sol
+++ contracts/VaultSpark.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract VaultSpark is Ownable, ReentrancyGuard {
+contract VaultSpark is Ownable(msg.sender), ReentrancyGuard {
     struct TokenInfo {
         bool supported;
         uint256 price; // price in wei per token
